f8425fd08d90d9aa4dc4595f580a8751af593c36
Misra, Deven E <deven.misra@pnnl.gov>
2023-02-27 11:49:18 -0800

    Added imaging calorimeter hits to dataset

--- gnn.py
+++ gnn.py
@@ -19,6 +19,7 @@ import uproot as ur
 import numpy as np
 import matplotlib.pyplot as plt
 from bitstring import BitArray
+import pandas as pd
 
 # %% [markdown]
 # Load Data from ROOT Files
@@ -59,9 +60,9 @@ def get_labels(data, count):
     energy_labels = []
     for i in range(count):
         label = np.sqrt(data["MCParticles.momentum.x"][0,0]**2 + data["MCParticles.momentum.y"][0,0]**2 + data["MCParticles.momentum.z"][0,0]**2)
-        if len(data["ZDC_PbSi_Hits.energy"][i]) != 0:
+        if len(data['ZDC_WSi_Hits.energy'][i]) > 0 or len(data['ZDC_PbSi_Hits.energy'][i]) > 0:
             energy_labels.append(label)
-    
+
     return energy_labels
 
 
@@ -71,7 +72,7 @@ def get_layerIDs(data, branch, events):
     for i in range(events):
         event_cellID = np.array(data[f"{branch}.cellID"][i])
         event_layerID = bitExtract(event_cellID, 6, 8)
-        if len(event_cellID) != 0:
+        if len(data['ZDC_WSi_Hits.energy'][i]) + len(data['ZDC_PbSi_Hits.energy'][i]) > 0:
             layerID.append(event_layerID)
             
     return layerID
@@ -82,7 +83,7 @@ def get_eDep(data, branch, events):
     hitsEnergy = []
     for i in range(events):
         event_hitsEnergy = np.array(data[f"{branch}.energy"][i])
-        if len(event_hitsEnergy) != 0:
+        if len(data['ZDC_WSi_Hits.energy'][i]) + len(data['ZDC_PbSi_Hits.energy'][i]) > 0:
             hitsEnergy.append(event_hitsEnergy)
 
     return hitsEnergy
@@ -94,8 +95,10 @@ def get_xIDs(data, branch, events):
     for i in range(events):
         event_cellID = np.array(data[f"{branch}.cellID"][i])
         event_xID = signedint(bitExtract(event_cellID, 12, 24))
-        if len(event_cellID) != 0:
+        if len(event_cellID) > 0:
             xID.append(event_xID)
+        elif len(data['ZDC_WSi_Hits.energy'][i]) > 0:
+            xID.append(np.array([]))
             
     return xID
 
@@ -107,11 +110,13 @@ def get_xIDs_WSi(data, branch, events):
         event_cellID = np.array(data[f"{branch}.cellID"][i])
         event_layerID = bitExtract(event_cellID, 6, 8)
         event_xID = signedint(bitExtract(event_cellID, 12, 24))
-        for i in range(len(event_cellID)):
+        for i in range(len(event_layerID)):
             if event_layerID[i] in [1, 12, 23]:
                 event_xID[i] = 0.3 * event_xID[i]
-        if len(event_xID) != 0:
+        if len(event_cellID) > 0:
             xID.append(event_xID)
+        elif len(data['ZDC_PbSi_Hits.energy'][i]) > 0:
+            xID.append(np.array([]))
             
     return xID
 
@@ -122,8 +127,10 @@ def get_yIDs(data, branch, events):
     for i in range(events):
         event_cellID = np.array(data[f"{branch}.cellID"][i])
         event_yID = signedint(bitExtract(event_cellID, 12, 36))
-        if len(event_cellID) != 0:
+        if len(event_cellID) > 0:
             yID.append(event_yID)
+        elif len(data['ZDC_WSi_Hits.energy'][i]) > 0:
+            yID.append(np.array([]))
             
     return yID
 
@@ -135,17 +142,22 @@ def get_yIDs_WSi(data, branch, events):
         event_cellID = np.array(data[f"{branch}.cellID"][i])
         event_layerID = bitExtract(event_cellID, 6, 8)
         event_yID = signedint(bitExtract(event_cellID, 12, 36))
-        for i in range(len(event_cellID)):
+        for i in range(len(event_layerID)):
             if event_layerID[i] in [1, 12, 23]:
                 event_yID[i] = 0.3 * event_yID[i]
-        if len(event_cellID) != 0:
+        if len(event_cellID) > 0:
             yID.append(event_yID)
-            
+        elif len(data['ZDC_PbSi_Hits.energy'][i]) > 0:
+            yID.append(np.array([]))
+
     return yID
 
 
 # %% [markdown]
 # Get Features and Labels
+
+# %%
+import pickle
 
 # %%
 nevents = 10000
@@ -157,34 +169,154 @@ yIDs = dict()
 layerIDs = dict()
 
 # %%
-hitEnergyDep['10GeV'] = get_eDep(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-xIDs['10GeV'] = get_xIDs(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-yIDs['10GeV'] = get_yIDs(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-layerIDs['10GeV'] = get_layerIDs(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+eDep_WSi_10 = get_eDep(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+eDep_PbSi_10 = get_eDep(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+xIDs_WSi_10 = get_xIDs_WSi(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+xIDs_PbSi_10 = get_xIDs(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+yIDs_WSi_10 = get_yIDs_WSi(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+yIDs_PbSi_10 = get_yIDs(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+layerIDs_WSi_10 = get_layerIDs(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+layerIDs_PbSi_10 = [x + 23 for x in get_layerIDs(samples["zdc_neutron_10GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)]
 
 # %%
-hitEnergyDep['20GeV'] = get_eDep(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-xIDs['20GeV'] = get_xIDs(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-yIDs['20GeV'] = get_yIDs(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-layerIDs['20GeV'] = get_layerIDs(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+hitEnergyDep['10GeV'] = [np.concatenate([eDep_WSi_10[i], eDep_PbSi_10[i]]) for i in range(len(eDep_WSi_10))]
+xIDs['10GeV'] = [np.concatenate([xIDs_WSi_10[i], xIDs_PbSi_10[i]]) for i in range(len(eDep_WSi_10))]
+yIDs['10GeV'] = [np.concatenate([yIDs_WSi_10[i], yIDs_PbSi_10[i]]) for i in range(len(eDep_WSi_10))]
+layerIDs['10GeV'] = [np.concatenate([layerIDs_WSi_10[i], layerIDs_PbSi_10[i]]) for i in range(len(eDep_WSi_10))]
+
+with open('hitEnergyDep_10GeV', 'wb') as handle:
+    pickle.dump(hitEnergyDep, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('xIDs_10GeV', 'wb') as handle:
+    pickle.dump(xIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('yIDs_10GeV', 'wb') as handle:
+    pickle.dump(yIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('layerIDs_10GeV', 'wb') as handle:
+    pickle.dump(layerIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 # %%
-hitEnergyDep['50GeV'] = get_eDep(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-xIDs['50GeV'] = get_xIDs(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-yIDs['50GeV'] = get_yIDs(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-layerIDs['50GeV'] = get_layerIDs(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+eDep_WSi_20 = get_eDep(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+eDep_PbSi_20 = get_eDep(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+xIDs_WSi_20 = get_xIDs_WSi(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+xIDs_PbSi_20 = get_xIDs(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+yIDs_WSi_20 = get_yIDs_WSi(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+yIDs_PbSi_20 = get_yIDs(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+layerIDs_WSi_20 = get_layerIDs(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+layerIDs_PbSi_20 = [x + 23 for x in get_layerIDs(samples["zdc_neutron_20GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)]
 
 # %%
-hitEnergyDep['100GeV'] = get_eDep(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-xIDs['100GeV'] = get_xIDs(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-yIDs['100GeV'] = get_yIDs(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-layerIDs['100GeV'] = get_layerIDs(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+hitEnergyDep['20GeV'] = [np.concatenate([eDep_WSi_20[i], eDep_PbSi_20[i]]) for i in range(len(eDep_WSi_20))]
+xIDs['20GeV'] = [np.concatenate([xIDs_WSi_20[i], xIDs_PbSi_20[i]]) for i in range(len(eDep_WSi_20))]
+yIDs['20GeV'] = [np.concatenate([yIDs_WSi_20[i], yIDs_PbSi_20[i]]) for i in range(len(eDep_WSi_20))]
+layerIDs['20GeV'] = [np.concatenate([layerIDs_WSi_20[i], layerIDs_PbSi_20[i]]) for i in range(len(eDep_WSi_20))]
+
+with open('hitEnergyDep_20GeV', 'wb') as handle:
+    pickle.dump(hitEnergyDep, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('xIDs_20GeV', 'wb') as handle:
+    pickle.dump(xIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('yIDs_20GeV', 'wb') as handle:
+    pickle.dump(yIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('layerIDs_20GeV', 'wb') as handle:
+    pickle.dump(layerIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 # %%
-hitEnergyDep['150GeV'] = get_eDep(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-xIDs['150GeV'] = get_xIDs(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-yIDs['150GeV'] = get_yIDs(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
-layerIDs['150GeV'] = get_layerIDs(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+eDep_WSi_50 = get_eDep(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+eDep_PbSi_50 = get_eDep(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+xIDs_WSi_50 = get_xIDs_WSi(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+xIDs_PbSi_50 = get_xIDs(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+yIDs_WSi_50 = get_yIDs_WSi(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+yIDs_PbSi_50 = get_yIDs(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+layerIDs_WSi_50 = get_layerIDs(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+layerIDs_PbSi_50 = [x + 23 for x in get_layerIDs(samples["zdc_neutron_50GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)]
+
+# %%
+hitEnergyDep['50GeV'] = [np.concatenate([eDep_WSi_50[i], eDep_PbSi_50[i]]) for i in range(len(eDep_WSi_50))]
+xIDs['50GeV'] = [np.concatenate([xIDs_WSi_50[i], xIDs_PbSi_50[i]]) for i in range(len(eDep_WSi_50))]
+yIDs['50GeV'] = [np.concatenate([yIDs_WSi_50[i], yIDs_PbSi_50[i]]) for i in range(len(eDep_WSi_50))]
+layerIDs['50GeV'] = [np.concatenate([layerIDs_WSi_50[i], layerIDs_PbSi_50[i]]) for i in range(len(eDep_WSi_50))]
+
+with open('hitEnergyDep_50GeV', 'wb') as handle:
+    pickle.dump(hitEnergyDep, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('xIDs_50GeV', 'wb') as handle:
+    pickle.dump(xIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('yIDs_50GeV', 'wb') as handle:
+    pickle.dump(yIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('layerIDs_50GeV', 'wb') as handle:
+    pickle.dump(layerIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# %%
+eDep_WSi_100 = get_eDep(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+eDep_PbSi_100 = get_eDep(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+xIDs_WSi_100 = get_xIDs_WSi(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+xIDs_PbSi_100 = get_xIDs(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+yIDs_WSi_100 = get_yIDs_WSi(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+yIDs_PbSi_100 = get_yIDs(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+layerIDs_WSi_100 = get_layerIDs(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+layerIDs_PbSi_100 = [x + 23 for x in get_layerIDs(samples["zdc_neutron_100GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)]
+
+# %%
+hitEnergyDep['100GeV'] = [np.concatenate([eDep_WSi_100[i], eDep_PbSi_100[i]]) for i in range(len(eDep_WSi_100))]
+xIDs['100GeV'] = [np.concatenate([xIDs_WSi_100[i], xIDs_PbSi_100[i]]) for i in range(len(eDep_WSi_100))]
+yIDs['100GeV'] = [np.concatenate([yIDs_WSi_100[i], yIDs_PbSi_100[i]]) for i in range(len(eDep_WSi_100))]
+layerIDs['100GeV'] = [np.concatenate([layerIDs_WSi_100[i], layerIDs_PbSi_100[i]]) for i in range(len(eDep_WSi_100))]
+
+with open('hitEnergyDep_100GeV', 'wb') as handle:
+    pickle.dump(hitEnergyDep, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('xIDs_100GeV', 'wb') as handle:
+    pickle.dump(xIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('yIDs_100GeV', 'wb') as handle:
+    pickle.dump(yIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('layerIDs_100GeV', 'wb') as handle:
+    pickle.dump(layerIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# %%
+eDep_WSi_150 = get_eDep(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+eDep_PbSi_150 = get_eDep(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+xIDs_WSi_150 = get_xIDs_WSi(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+xIDs_PbSi_150 = get_xIDs(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+yIDs_WSi_150 = get_yIDs_WSi(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+yIDs_PbSi_150 = get_yIDs(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)
+layerIDs_WSi_150 = get_layerIDs(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_WSi_Hits", nevents)
+layerIDs_PbSi_150 = [x + 23 for x in get_layerIDs(samples["zdc_neutron_150GeV_10e4.edm4hep.root"], "ZDC_PbSi_Hits", nevents)]
+
+# %%
+hitEnergyDep['150GeV'] = [np.concatenate([eDep_WSi_150[i], eDep_PbSi_150[i]]) for i in range(len(eDep_WSi_150))]
+xIDs['150GeV'] = [np.concatenate([xIDs_WSi_150[i], xIDs_PbSi_150[i]]) for i in range(len(eDep_WSi_150))]
+yIDs['150GeV'] = [np.concatenate([yIDs_WSi_150[i], yIDs_PbSi_150[i]]) for i in range(len(eDep_WSi_150))]
+layerIDs['150GeV'] = [np.concatenate([layerIDs_WSi_150[i], layerIDs_PbSi_150[i]]) for i in range(len(eDep_WSi_150))]
+
+with open('hitEnergyDep_150GeV', 'wb') as handle:
+    pickle.dump(hitEnergyDep, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('xIDs_150GeV', 'wb') as handle:
+    pickle.dump(xIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('yIDs_150GeV', 'wb') as handle:
+    pickle.dump(yIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('layerIDs_150GeV', 'wb') as handle:
+    pickle.dump(layerIDs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# %%
+with open('hitEnergyDep_150GeV', 'rb') as handle:
+    hitEnergyDep = pickle.load(handle)
+with open('xIDs_150GeV', 'rb') as handle:
+    xIDs = pickle.load(handle)
+with open('yIDs_150GeV', 'rb') as handle:
+    yIDs = pickle.load(handle)
+with open('layerIDs_150GeV', 'rb') as handle:
+    layerIDs = pickle.load(handle)
 
 # %% [markdown]
 # Merge Data
@@ -203,7 +335,7 @@ yIDs_all = ak.concatenate(list(yIDs.values()), axis=0)
 layerIDs_all = ak.concatenate(list(layerIDs.values()), axis=0)
 
 # %%
-hitEnergyDep_train, hitEnergyDep_test, xIDs_train, xIDs_test, yIDs_train, yIDs_test, layerIDs_train, layerIDs_test, labels_train, labels_test = train_test_split(hitEnergyDep_all, xIDs_all, yIDs_all, layerIDs_all, data_labels, test_size=0.2, train_size=0.8, random_state=42, shuffle=True)
+hitEnergyDep_train, hitEnergyDep_test, xIDs_train, xIDs_test, yIDs_train, yIDs_test, layerIDs_train, layerIDs_test, labels_train, labels_test = train_test_split(hitEnergyDep_all, xIDs_all, yIDs_all, layerIDs_all, data_labels, test_size=0.2, train_size=0.8, random_state=None, shuffle=True)
 
 # %%
 features_train = [hitEnergyDep_train, xIDs_train, yIDs_train, layerIDs_train]
@@ -218,9 +350,6 @@ from torch import nn
 from torch_geometric.data import Data, Dataset
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import knn_graph, GCNConv, global_add_pool
-
-# %%
-torch.manual_seed(42)
 
 
 # %%
@@ -251,7 +380,7 @@ class ZDCDataset(Dataset):
         x = torch.stack([energy, xID, yID, layerID], axis=-1)
         
         #construct knn graph from (x, y, z) coordinates
-        edge_index = knn_graph(x[:, [1,3]], k=self.knn_k, num_workers=48)
+        edge_index = knn_graph(x[:, [1,3]], k=self.knn_k, num_workers=32)
         
         data = Data(
             x = x,
@@ -264,10 +393,15 @@ class ZDCDataset(Dataset):
 
 # %%
 dataset = ZDCDataset(features_train, labels_train, knn_k=4)
-loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=48)
+loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers = 32)
 
-test_dataset = ZDCDataset(features_test, labels_test, knn_k=4)
-test_loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=48)
+# %%
+ibatch = 0
+for data_batched in loader:
+    print(ibatch, data_batched.x.shape, data_batched.y)
+    ibatch += 1
+    if ibatch>5:
+        break
 
 # %% [markdown]
 # GCN Model
@@ -279,11 +413,11 @@ class Net(torch.nn.Module):
     def __init__(self, num_node_features=4):
         super(Net, self).__init__()
         
-        #(4 -> 64)
-        self.conv1 = GCNConv(num_node_features, 64)
+        #(4 -> N)
+        self.conv1 = GCNConv(num_node_features, 256)
         
-        #(64 -> 1)
-        self.output = torch.nn.Linear(64, 1)
+        #(N -> 1)
+        self.output = torch.nn.Linear(256, 1)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
@@ -307,6 +441,12 @@ class Net(torch.nn.Module):
 
 
 # %%
+net = Net()
+
+# %%
+net(data_batched)
+
+# %%
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = Net().to(device)
@@ -316,6 +456,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 #scheduler = ExponentialLR(optimizer, gamma=0.9)
 
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+
+# %%
+torch.set_num_threads(32)
 
 # %%
 model.train()
@@ -345,6 +488,9 @@ for epoch in range(1000):
         torch.save(obj=model.state_dict(), f=f"/home/dmisra/eic/gnn_state_dict_{epoch}")
 
 # %%
+#model.load_state_dict(torch.load('/home/dmisra/eic/gnn_state_dict_300'))
+
+# %%
 plt.plot(losses_train, label="training")
 plt.ylabel("Loss")
 plt.xlabel("epoch")
@@ -353,6 +499,13 @@ plt.xlabel("epoch")
 ievent = 42
 data = dataset.get(ievent).to(device)
 embedded_nodes = model.conv1(data.x, data.edge_index)
+
+# %%
+test_dataset = ZDCDataset(features_test, labels_test, knn_k=4)
+test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=True, num_workers = 32)
+
+# %%
+next(iter(test_loader))
 
 # %% [markdown]
 # Predictions
@@ -367,7 +520,7 @@ model.eval()
 
 #Setup the inference mode context manager
 with torch.inference_mode():
-  y_preds = model()
+  y_preds = model(next(iter(test_loader)))
 
 plt.hist(y_preds[:,0].numpy(),100,histtype='step')
 plt.xlabel('Energy (GeV)')
